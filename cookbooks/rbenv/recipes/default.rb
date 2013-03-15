62812787ec6378eea5b0d0651af53fa2e07b09ed
masudaK <masuken@gmail.com>
2013-03-16 03:38:28 +0900

    modify test

--- cookbooks/rbenv/recipes/default.rb
+++ cookbooks/rbenv/recipes/default.rb
@@ -2,10 +2,30 @@
 # Cookbook Name:: rbenv
 # Recipe:: default
 #
-# Copyright 2013, YOUR_COMPANY_NAME
+# Copyright 2013, Kenichi MASUDA
 #
 # All rights reserved - Do Not Redistribute
 #
-package "ruby" do
-  action :install
+
+owner_name = 'masuda_kenichi'
+
+git "/home/#{owner_name}/.rbenv" do
+  repository "https://github.com/sstephenson/rbenv.git"
+  reference "master"
+  action :checkout
+  not_if "test -d /home/#{owner_name}/.rbenv"
+end
+
+directory "/home/#{owner_name}/.rbenv/plugins" do
+  user "#{owner_name}"
+  group "#{owner_name}"
+  mode 0755
+  action :create
+end
+
+git "/home/#{owner_name}/.rbenv/plugins/ruby-build" do
+  repository "https://github.com/sstephenson/ruby-build.git"
+  reference "master"
+  action :checkout
+  not_if "test -d /home/#{owner_name}/.rbenv/plugins/ruby-build"
 end
--- cookbooks/rbenv/spec/default_spec.rb
+++ cookbooks/rbenv/spec/default_spec.rb
@@ -2,8 +2,19 @@ require 'chefspec'
 
 describe 'rbenv::default' do
   let (:chef_run) { ChefSpec::ChefRunner.new.converge 'rbenv::default' }
-  it '"ruby" package should be installed' do
-    expect(chef_run).to install_package 'ruby'
-#    expect(chef_run).to execute_command 'ruby -v'
+#  it 'should be cloned' do
+#    # TODO below change test way for scm resources
+#    expect(chef_run).to create_directory "/home/masuda_kenichi/.rbenv"
+#  end
+
+  it 'should create ruby-build dir' do
+    chef_run.should create_directory '/home/masuda_kenichi/.rbenv/plugins'
+    chef_run.directory('/home/masuda_kenichi/.rbenv/plugins').should be_owned_by('masuda_kenichi', 'masuda_kenichi')
+    chef_run.directory('/home/masuda_kenichi/.rbenv/plugins').mode.should == 0755
   end
+
+#  it 'should be cloned' do
+#    # TODO below change test way for scm resources
+#    expect(chef_run).to create_directory "/home/masuda_kenichi/.rbenv"
+#  end
 end
