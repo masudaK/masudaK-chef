1ea700bb8be9750d81a25077da7727715f8fc983
masudaK <masuken@gmail.com>
2013-03-16 02:35:56 +0900

    modify cucumber steps

--- features/step_defenitions/rbenv_step.rb
+++ features/step_defenitions/rbenv_step.rb
@@ -1,6 +1,23 @@
 # encoding: UTF-8
 
-前提(/^: "(.*?)"と実行する$/) do |exit_status|
-  @last_exit_status.should == exit_status.to_i
+前提(/^: "(.*?)"ができている$/) do |file|
+  check_file_presence([file], true)
 end
 
+ならば(/^: rbenvコマンドが使えるようになっている$/) do
+  run_interactive(unescape('rbenv'))
+end
+
+前提(/^: "(.*?)"と実行する$/) do |cmd|
+  @execute_command='ruby -v'
+  unless @execute_command.nil?
+    steps %Q{ When I run `#{@execute_command} "#{cmd}"` }
+  else
+    @execute_failed=true
+    raise "No input command specified"
+  end
+end
+
+ならば(/^: 終了ステータスが(\d+)だ$/) do |expected|
+  assert_partial_output(expected, all_output)
+end
