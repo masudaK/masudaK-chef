8db142102cbf6a36ecded470178d5c7025009e2c
masudaK <masuken@gmail.com>
2013-03-20 13:47:38 +0900

    kichenfile modify for new target os

--- cookbooks/rbenv/files/default/tests/minitest/default_test.rb
+++ cookbooks/rbenv/files/default/tests/minitest/default_test.rb
@@ -2,18 +2,22 @@
 require 'minitest/spec'
 
 describe 'rbenv' do
-#  require 'chef/mixin/shell_out'
-#  include Chef::Mixin::ShellOut
   include MiniTest::Chef::Assertions
   include MiniTest::Chef::Context
   include MiniTest::Chef::Resources
 
 
    it 'installs git' do
-     package('git').must_be_installed.with(:version, "1.7.1-3.el6_4.1") # minitest-chef-handler
+     package('git').must_be_installed.with(:version, "11.7.1-3.el6_4.1") # minitest-chef-handler
    end
-   it "Directory dir" do
-     file("/home/vagrant/.rbenv/plugins").must_exist.with(:owner, "vagrant").and(:group, "vagrant").and(:mode, "0755")
+   it "clone rbenv.git" do
+     file("1/home/vagrant/.rbenv/bin/rbenv").must_exist.with(:owner, "vagrant").and(:group, "vagrant")
+   end
+   it "Directory .rbenv/plugins" do
+     file("1/home/vagrant/.rbenv/plugins").must_exist.with(:owner, "vagrant").and(:group, "vagrant").and(:mode, "0755")
+   end
+   it "clone ruby-build.git" do
+     file("1/home/vagrant/.rbenv/plugins/ruby-build/bin/ruby-build").must_exist.with(:owner, "vagrant").and(:group, "vagrant")
    end
 
 end
